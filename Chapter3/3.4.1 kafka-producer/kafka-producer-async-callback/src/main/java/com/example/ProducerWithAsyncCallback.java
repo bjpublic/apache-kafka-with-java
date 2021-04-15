026a1bce5f4e57825d7d277809c15707d8824cdd
AndersonChoi <choco4235@gmail.com>
2021-04-15 13:41:13 +0900

    Update ProducerWithAsyncCallback.java

--- Chapter3/3.4.1 kafka-producer/kafka-producer-async-callback/src/main/java/com/example/ProducerWithAsyncCallback.java
+++ Chapter3/3.4.1 kafka-producer/kafka-producer-async-callback/src/main/java/com/example/ProducerWithAsyncCallback.java
@@ -1,6 +1,5 @@
 package com.example;
 
-import com.google.gson.Gson;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
