f646685688fd0e6e6b0792255f41765215796a11
choco4235 <choco4235@gmail.com>
2023-03-26 21:31:28 +0900

    my-kafka, test토픽 수정

--- Chapter3/3.4.1 kafka-producer/simple-kafka-producer/src/main/java/com/example/SimpleProducer.java
+++ Chapter3/3.4.1 kafka-producer/simple-kafka-producer/src/main/java/com/example/SimpleProducer.java
@@ -11,8 +11,8 @@ import java.util.Properties;
 
 public class SimpleProducer {
     private final static Logger logger = LoggerFactory.getLogger(SimpleProducer.class);
-    private final static String TOPIC_NAME = "test2";
-    private final static String BOOTSTRAP_SERVERS = "localhost:9092";
+    private final static String TOPIC_NAME = "test";
+    private final static String BOOTSTRAP_SERVERS = "my-kafka:9092";
 
     public static void main(String[] args) {
 
